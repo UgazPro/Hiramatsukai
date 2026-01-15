08077c7afc4893bc6128c3d850cceb0111c557e5
UgazPro <ugazdesu@gmail.com>
2026-01-15 15:58:32 -0400

    Add Header component to AdminLayout for improved structure

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router"
 import AdminSidebar from "./AdminSidebar/AdminSidebar"
 import Footer from "./Footer"
+import Header from "./Header"
 
 export default function AdminLayout() {
     return (
@@ -8,6 +9,7 @@ export default function AdminLayout() {
             <div className="flex relative h-full min-h-screen w-full bg-gray-200 justify-end">
                 <AdminSidebar />
                 <div className="h-full w-full pl-12">
+                    <Header />
                     <Outlet />
                     <Footer />
                 </div>
