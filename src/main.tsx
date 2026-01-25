b33f41b39c065192da953b57f7cebbf76ea15801
UgazPro <ugazdesu@gmail.com>
2026-01-25 17:24:22 -0400

    refactor: Integrate QueryClientProvider in main.tsx for React Query setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const clientId = import.meta.env.VITE_CLIENT_ID_GOOGLE;
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
 )
