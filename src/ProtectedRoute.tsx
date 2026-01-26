ec5e7f6136c3999b14d3cf9fbd4049fec8bc4a09
UgazPro <ugazdesu@gmail.com>
2026-01-25 23:00:55 -0400

    feat: implement ProtectedRoute component for route protection and remove unused token helper functions

---
+++ src/ProtectedRoute.tsx
@@ -0,0 +1,31 @@
+// ProtectedRoute.tsx
+import { ReactNode, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useAuthStore } from "@/stores/auth.store";
+import SpinnerComponent from "@/components/SpinnerComponent";
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+
+  const token = useAuthStore((state) => state.token);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/", { replace: true });
+    }
+  }, [token, navigate]);
+
+  if (!token) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <SpinnerComponent />
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+}
